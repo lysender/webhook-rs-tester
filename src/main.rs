0a38085f3f350cafccb0c38bfeaec50ecfbe8550
Lysender <lysender@darkstar-win>
2024-09-29 19:27:17 +0800

    fix add simple post payload tester

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
+use reqwest::Client;
 use serde::Serialize;
-use tracing::{error, info};
-
-mod error;
-
-pub use error::{Error, Result};
 
 const APP_URL: &'static str = "http://localhost:4200/webhook";
 
@@ -14,27 +10,22 @@ struct CounterPayload {
 
 #[tokio::main]
 async fn main() {
-    // Set the RUST_LOG, if it hasn't been explicitly defined
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "webhook_rs_tester=info")
-    }
+    let client = reqwest::Client::new();
 
-    tracing_subscriber::fmt()
-        .with_target(false)
-        .compact()
-        .init();
-
-    let handles = (0..20).map(|id| tokio::spawn(async move { send_request(id).await }));
+    let handles = (0..100).map(|id| {
+        let client_copy = client.clone();
+        tokio::spawn(async move { send_request(client_copy, id).await })
+    });
 
     for handle in handles {
         handle.await.unwrap();
     }
 }
 
-async fn send_request(id: usize) {
-    let client = reqwest::Client::new();
+async fn send_request(client: Client, id: usize) {
     let data = CounterPayload { id };
     let post_body = serde_json::to_string(&data).unwrap();
+
     let response = client
         .post(APP_URL)
         .header("Content-Type", "application/json")
@@ -42,10 +33,11 @@ async fn send_request(id: usize) {
         .send()
         .await
         .unwrap();
+
     if response.status().is_success() {
         let res_body = response.text().await.unwrap();
-        info!("Request: {}, Response: {}", id, res_body);
+        println!("Request: {}, Response: {}", id, res_body);
     } else {
-        error!("Request: {} failed.", id);
+        println!("Request: {} failed.", id);
     }
 }
